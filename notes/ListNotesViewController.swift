af431e5f9fbdd52b3b99bb701e1d9fb43ed3965d
Mahdi <mahdichtioui19@gmail.com>
2019-11-08 12:29:53 +0100

    List Notes ViewController creation

---
+++ notes/ListNotesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListNotesViewController.swift
+//  notes
+//
+//  Created by iOS Dev on 11/8/19.
+//  Copyright © 2019 iOS Dev. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = "Try Merging This!!!"
+    }
+
+}
